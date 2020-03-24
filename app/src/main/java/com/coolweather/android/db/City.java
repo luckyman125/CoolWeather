ef10314e1b5f3e030e57700cf77a41e4afabcb7a
41704949 <2271196354@qq.com>
2020-03-24 08:08:06 +0000

    ADD fragment.

---
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -0,0 +1,41 @@
+package com.coolweather.android.db;
+
+public class City {
+    private int id;
+    private String cityName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    private int cityCode;
+    private int provinceCode;
+}
